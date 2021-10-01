bf5817c0a856b2592eb3b3d45e5f443f868b8b89
pocopico <pasafiogos@gmail.com>
2021-10-01 17:26:50 +0300

    Fix things

---
+++ tg3/src/check-tg3.sh
@@ -0,0 +1,10 @@
+#
+# Checking modules is loaded
+#
+
+echo -n "Loading module tg3 -> "
+
+if [ $(/bin/lsmod |grep -i tg3|wc -l) -eq 1 ] ; then 
+	echo "Module Tigon 3 loaded succesfully" 
+	else echo "Module Tigon 3 is not loaded "
+fi
