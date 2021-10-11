534cf19320f3050f82e870bd14a8cb66da01ea96
pocopico <pasafiogos@gmail.com>
2021-10-11 19:16:40 +0300

    Realtek RTL8152 Based USB 2.0 Ethernet Adapters

---
+++ r8152/src/check-r8152.sh
@@ -0,0 +1,10 @@
+#
+# Checking modules is loaded
+#
+
+echo -n "Loading module r8152 -> "
+
+if [ 0 -eq 1 ] ; then
+        echo "Module r8152 loaded succesfully"
+        else echo "Module r8152 is not loaded "
+fi
