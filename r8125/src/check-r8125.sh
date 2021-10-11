ee5119b4f4d98bb4bc7c00cf5bef2bff2c10f0ef
pocopico <pasafiogos@gmail.com>
2021-10-11 19:13:16 +0300

    Added r8125 support

---
+++ r8125/src/check-r8125.sh
@@ -0,0 +1,10 @@
+#
+# Checking modules is loaded
+#
+
+echo -n "Loading module r8125 -> "
+
+if [ 0 -eq 1 ] ; then
+        echo "Module r8125 loaded succesfully"
+        else echo "Module r8125 is not loaded "
+fi
