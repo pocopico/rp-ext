7888261e9b36eaa3eb9d82fc10290f9c63abd8e6
pocopico <pasafiogos@gmail.com>
2021-10-11 14:07:17 +0300

    Added Latest Intel e1000e and igb modules

---
+++ e1000e/src/check-e1000e.sh
@@ -0,0 +1,10 @@
+#
+# Checking modules is loaded
+#
+
+echo -n "Loading module e1000e -> "
+
+if [ 0 -eq 1 ] ; then
+        echo "Module e1000e loaded succesfully"
+        else echo "Module e1000e is not loaded "
+fi
---
+++ igb/src/check-igb.sh
@@ -0,0 +1,10 @@
+#
+# Checking modules is loaded
+#
+
+echo -n "Loading module igb -> "
+
+if [ 0 -eq 1 ] ; then
+        echo "Module igb loaded succesfully"
+        else echo "Module igb is not loaded "
+fi
