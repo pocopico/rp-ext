76d231ed2f041953b0462ab5988d963e962f33a6
pocopico <pasafiogos@gmail.com>
2021-10-11 19:33:04 +0300

    Davicom DM9601 USB 1.1 ethernet devices

---
+++ dm9601/src/check-dm9601.sh
@@ -0,0 +1,10 @@
+#
+# Checking modules is loaded
+#
+
+echo -n "Loading module dm9601 -> "
+
+if [ 0 -eq 1 ] ; then
+        echo "Module dm9601 loaded succesfully"
+        else echo "Module dm9601 is not loaded "
+fi
