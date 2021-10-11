664463aa2499e0e03178783fc773af2956e60340
pocopico <pasafiogos@gmail.com>
2021-10-11 19:35:13 +0300

    rtl8150 based usb-ethernet driver

---
+++ rtl8150/src/check-rtl8150.sh
@@ -0,0 +1,10 @@
+#
+# Checking modules is loaded
+#
+
+echo -n "Loading module rtl8150 -> "
+
+if [ 0 -eq 1 ] ; then
+        echo "Module rtl8150 loaded succesfully"
+        else echo "Module rtl8150 is not loaded "
+fi
