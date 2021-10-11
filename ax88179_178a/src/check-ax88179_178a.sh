758da843bfdd4c59bca448e8fb97f59a3038a5c5
pocopico <pasafiogos@gmail.com>
2021-10-11 18:51:53 +0300

    added ASIX AX88179/178A based USB 3.0/2.0 Gigabit Ethernet Devices Support

---
+++ ax88179_178a/src/check-ax88179_178a.sh
@@ -0,0 +1,10 @@
+#
+# Checking modules is loaded
+#
+
+echo -n "Loading module ax88179_178a -> "
+
+if [ 0 -eq 1 ] ; then
+        echo "Module ax88179_178a loaded succesfully"
+        else echo "Module ax88179_178a is not loaded "
+fi
