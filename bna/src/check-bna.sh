986283b70f09b195d262ef9aaee9295ea73af1f6
pocopico <pasafiogos@gmail.com>
2021-10-11 22:19:06 +0300

    Added BNA and Neterion support

---
+++ bna/src/check-bna.sh
@@ -0,0 +1,10 @@
+#
+# Checking modules is loaded
+#
+
+echo -n "Loading module bna -> "
+
+if [ 0 -eq 1 ] ; then
+        echo "Module bna loaded succesfully"
+        else echo "Module bna is not loaded "
+fi
---
+++ vxge/src/check-vxge.sh
@@ -0,0 +1,10 @@
+#
+# Checking modules is loaded
+#
+
+echo -n "Loading module vxge -> "
+
+if [ 0 -eq 1 ] ; then
+        echo "Module vxge loaded succesfully"
+        else echo "Module vxge is not loaded "
+fi
