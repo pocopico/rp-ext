7c0f44f49c8439e3db9466567702400fb57507dd
pocopico <pasafiogos@gmail.com>
2021-10-12 13:57:58 +0300

    Added Tehuti Network Driver and version : 0.3.6.14

---
+++ tn40xx/src/check-tn40xx.sh
@@ -0,0 +1,10 @@
+#
+# Checking modules is loaded
+#
+
+echo -n "Loading module tn40xx -> "
+
+if [ 0 -eq 1 ] ; then
+        echo "Module tn40xx loaded succesfully"
+        else echo "Module tn40xx is not loaded "
+fi
