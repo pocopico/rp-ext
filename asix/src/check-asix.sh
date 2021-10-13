d7a8777d3c9f33b52192934be90d557c0f1e14db
pocopico <pasafiogos@gmail.com>
2021-10-13 10:58:22 +0300

    Updated to add 4.4.59 (6.2.4) releases

---
+++ asix/src/check-asix.sh
@@ -0,0 +1,10 @@
+#
+# Checking modules is loaded
+#
+
+echo -n "Loading module asix -> "
+
+if [ 0 -eq 1 ] ; then
+        echo "Module asix loaded succesfully"
+        else echo "Module asix is not loaded "
+fi
