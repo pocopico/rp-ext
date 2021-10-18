fd24f4d9ccfea5b494aaa04d88da912834474538
pocopico <pasafiogos@gmail.com>
2021-10-18 13:06:23 +0300

    Stripped Modules

--- mptsas/src/check-mptsas.sh
+++ mptsas/src/check-mptsas.sh
@@ -2,9 +2,9 @@
 # Checking modules is loaded
 #
 
-echo -n "Loading module MPT SAS -> "
+echo -n "Loading module mptsas -> "
 
-if [ $(/sbin/lsmod |grep -i mptsas|wc -l) -gt 0 ] ; then 
-	echo "Module MPT SAS loaded succesfully" 
-	else echo "Module MPTSAS is not loaded "
+if [ `/sbin/lsmod |grep -i mptsas|wc -l` -eq 1 ] ; then
+        echo "Module mptsas loaded succesfully"
+        else echo "Module mptsas is not loaded "
 fi
