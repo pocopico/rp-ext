c1fac9d06645b5abb2c1eecf2f99df8a643ff3f4
pocopico <pasafiogos@gmail.com>
2021-10-18 23:13:52 +0300

    Recompiled all 4.4.180 modules

--- tg3/src/check-tg3.sh
+++ tg3/src/check-tg3.sh
@@ -4,7 +4,7 @@
 
 echo -n "Loading module tg3 -> "
 
-if [ $(/sbin/lsmod |grep -i tg3|wc -l) -eq 1 ] ; then 
-	echo "Module Tigon 3 loaded succesfully" 
-	else echo "Module Tigon 3 is not loaded "
+if [ `/sbin/lsmod |grep -i tg3|wc -l` -eq 1 ] ; then
+        echo "Module tg3 loaded succesfully"
+        else echo "Module tg3 is not loaded "
 fi
