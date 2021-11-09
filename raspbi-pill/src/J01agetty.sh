e5099fe321783efb15b26b07f4ad913ed7815895
pocopico <pasafiogos@gmail.com>
2021-11-10 01:02:07 +0200

    Updated raspbi-pill

--- raspbi-pill/src/J01agetty.sh
+++ raspbi-pill/src/J01agetty.sh
@@ -13,22 +13,8 @@ EOF
 }
 
 start() {
-
         echo "---------={ Starting agetty service }=---------"
-
-if [ `who -u |grep -i ttyACM0| grep -v grep | wc -l ` -eq 1 ] ;
-then
-echo "User looged in from Serial Terminal waiting for exit"
-else
-        if [ `ps -ef |grep -ie ttyACM0 |grep -v grep | wc -l` -eq 1 ];
-        then
-        echo "agetty running"
-        else
-        echo "Starting agetty"
-        /sbin/agetty --local-line 115200 ttyACM0 vt100
-        fi
-fi
-
+	/sbin/agettystart.sh &
 
 }
 stop() {
--- raspbi-pill/src/raspbi-pill.sh
+++ raspbi-pill/src/raspbi-pill.sh
@@ -19,5 +19,7 @@
         echo "---------={ Starting agetty service }=---------"
 	/bin/cp agetty /sbin/
 	/bin/cp J01agetty.sh /usr/syno/etc/rc.d/
+	/bin/cp agettystart.sh /sbin/
 	chmod 777 /sbin/agetty 
 	chmod 777 /usr/syno/etc/rc.d/J01agetty.sh
+	chmod 777 /sbin/agettystart.sh
