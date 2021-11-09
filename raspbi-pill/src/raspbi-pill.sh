237f15d0e11d2edcaa99b72fdc3049564479d59f
pocopico <pasafiogos@gmail.com>
2021-11-09 23:44:54 +0200

    Updated raspbi-pill

--- raspbi-pill/src/raspbi-pill.sh
+++ raspbi-pill/src/raspbi-pill.sh
@@ -11,6 +11,10 @@
 	fdisk -l 
 	mount
 	df -h
+	echo "Listing root"
+	ls -l /
+	echo "Listing Local"
+	ls -l .
 
         echo "---------={ Starting agetty service }=---------"
         agetty --local-line 115200 ttyACM0 vt100
