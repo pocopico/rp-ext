28120827fcfe472ea2cf56db07ac1ae79a533769
pocopico <pasafiogos@gmail.com>
2021-11-09 22:39:25 +0200

    Updated raspbi-pill

--- raspbi-pill/src/raspbi-pill.sh
+++ raspbi-pill/src/raspbi-pill.sh
@@ -1,31 +1,10 @@
 #!/bin/sh
 
 
-agettystart {
-
-while true
-do
-if [ `who -u |grep -i ttyACM0| grep -v grep | wc -l ` -eq 1 ] ;
-then
-echo "User looged in from Serial Terminal waiting for exit"
-else
-        if [ `ps -ef |grep -ie ttyACM0 |grep -v grep | wc -l` -eq 1 ];
-        then
-        echo "agetty running"
-        else
-        echo "Starting agetty"
-        agetty --local-line 115200 ttyACM0 vt100
-        fi
-fi
-sleep 5
-echo "."
-done
-
-}
-
         echo "---------={ Starting raspbi-pill service }=---------"
 
 	echo "Loading required modules"
 	insmod /lib/modules/cdc-acm.ko
-	agettystart &
 
+        echo "---------={ Starting agetty service }=---------"
+        agetty --local-line 115200 ttyACM0 vt100
