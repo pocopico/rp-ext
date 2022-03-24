6691fd7a5edac38d913f13f7ae1b14be215fe5d2
pocopico <pasafiogos@gmail.com>
2022-03-24 15:43:05 +0200

    Added ixgbe.vanilla for 3615xs

---
+++ ixgbe.vanilla/src/check-ixgbe.sh
@@ -0,0 +1,10 @@
+#
+# Checking modules is loaded
+#
+
+echo -n "Loading module ixgbe -> "
+
+if [ `/sbin/lsmod |grep -i ixgbe|wc -l` -eq 1 ] ; then
+        echo "Module ixgbe loaded succesfully"
+        else echo "Module ixgbe is not loaded "
+fi
