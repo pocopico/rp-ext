9072796911521f0be00059c1d8e58706f4a63e41
pocopico <pasafiogos@gmail.com>
2022-07-11 18:05:24 +0300

    Changed redpill-dtb to dtbpatch

--- dtbpatch/releases/install.sh
+++ dtbpatch/releases/install.sh
@@ -1,20 +1,35 @@
 #!/usr/bin/env ash
 
-if [ "${1}" = "early" ]; then
+
+### USUALLY SCEMD is the last process run in init, so when scemd is running we are most
+# probably certain that system has finish init process
+#
+
+
+if [ `ps -ef |grep -i scemd |grep -v grep | wc -l` -gt 0 ] ; then
+HASBOOTED="yes"
+echo "System has completed init process"
+else
+echo "System is booting"
+HASBOOTED="no"
+fi
+
+
+if [ "$HASBOOTED" = "no" ]; then
   echo "dtbpatch - early"
   # fix executable flag
   cp dtbpatch /usr/sbin/
   chmod +x /usr/sbin/dtbpatch
 
   # Dynamic generation
-  /usr/bin/dtbpatch /etc.defaults/model.dtb output.dtb
+  /usr/sbin/dtbpatch /etc.defaults/model.dtb output.dtb
   if [ $? -ne 0 ]; then
     echo "Error patching dtb"
   else
     cp -vf output.dtb /etc.defaults/model.dtb
     cp -vf output.dtb /var/run/model.dtb
   fi
-elif [ "${1}" = "late" ]; then
+elif [ "$HASBOOTED" = "yes" ]; then
   echo "dtbpatch - late"
   # copy file
   cp -vf /etc.defaults/model.dtb /tmpRoot/etc.defaults/model.dtb
