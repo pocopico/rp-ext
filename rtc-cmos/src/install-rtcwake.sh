3822033bce407999aa4779b2e34c6521045dd1fe
pocopico <pasafiogos@gmail.com>
2022-10-25 16:31:48 +0300

    Added rtcwake monitoring functionality

--- rtc-cmos/src/install-rtcwake.sh
+++ rtc-cmos/src/install-rtcwake.sh
@@ -5,6 +5,7 @@ tar -zxvf rtcwake.tar.gz -C /tmpRoot/
 # enable
 #systemctl enable rtcwake.service
 #ln -s /usr/lib/systemd/system/rtcwake.service /tmpRoot/etc/systemd/system/multi-user.target.wants/rtcwake.service
+mkdir -p /tmpRoot/etc/systemd/system/syno-bootup-done.target.wants
 ln -s /usr/lib/systemd/system/rtcwake.service /tmpRoot/etc/systemd/system/syno-bootup-done.target.wants/rtcwake.service
 
 # start
