78272f802cea9adb2f93acaa305de9da861f7270
pocopico <pasafiogos@gmail.com>
2023-02-03 13:58:52 +0200

    Updated to latest available

--- eudev/releases/install.sh
+++ eudev/releases/install.sh
@@ -26,7 +26,31 @@ if [ "$HASBOOTED" = "no" ]; then
   udevadm trigger --type=devices --action=add
   udevadm settle --timeout=10 || echo "udevadm settle failed"
 
+
+  # Give more time
+  sleep 10
+  # Remove from memory to not conflict with RAID mount scripts
+  /usr/bin/killall udevd
 elif [ "$HASBOOTED" = "yes" ]; then
   echo "eudev - late"
+  # Copy rules
+  cp -vf /etc/udev/rules.d/* /tmpRoot/usr/lib/udev/rules.d/
+  DEST="/tmpRoot/lib/systemd/system/udevrules.service"
+
+  echo "[Unit]"                                                                  >${DEST}
+  echo "Description=Reload udev rules"                                          >>${DEST}
+  echo                                                                          >>${DEST}
+  echo "[Service]"                                                              >>${DEST}
+  echo "Type=oneshot"                                                           >>${DEST}
+  echo "RemainAfterExit=true"                                                   >>${DEST}
+  echo "ExecStart=/usr/bin/udevadm hwdb --update"                               >>${DEST}
+  echo "ExecStart=/usr/bin/udevadm control --reload-rules"                      >>${DEST}
+  echo                                                                          >>${DEST}
+  echo "[Install]"                                                              >>${DEST}
+  echo "WantedBy=multi-user.target"                                             >>${DEST}
+
+  mkdir -p /tmpRoot/etc/systemd/system/multi-user.target.wants
+  ln -sf /lib/systemd/system/udevrules.service /tmpRoot/lib/systemd/system/multi-user.target.wants/udevrules.service
 fi
+
 
